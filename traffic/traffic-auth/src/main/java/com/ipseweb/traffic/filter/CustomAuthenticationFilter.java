107804abce56e19710ac17205b672871038ef692
양승빈 (AI IoT플랫폼개발TF) <seungbin.yang@kt.com>
2024-11-03 22:17:13 +0900

    인증모듈 초기 세팅

---
+++ traffic/traffic-auth/src/main/java/com/ipseweb/traffic/filter/CustomAuthenticationFilter.java
@@ -0,0 +1,21 @@
+package com.ipseweb.traffic.filter;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private final AuthenticationManager authenticationManager;
+
+    public CustomAuthenticationFilter(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+//    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+//
+//    }
+}
