e4ff4aa147e2463282f2bc7ae970ac5204f4088b
양승빈 (AI IoT플랫폼개발TF) <seungbin.yang@kt.com>
2025-03-06 14:38:33 +0900

    - docker-compose 설정 추가  - nginx host port 변경  - api 모듈 context path 적용

--- traffic/traffic-api/src/main/java/com/ipseweb/traffic/resource/UrlResource.java
+++ traffic/traffic-api/src/main/java/com/ipseweb/traffic/resource/UrlResource.java
@@ -1,7 +1,7 @@
 package com.ipseweb.traffic.resource;
 
 public class UrlResource {
-    public static final String BASE_URL = "/api/traffic";
+    public static final String BASE_URL = "/traffic";
 
-    public static final String BASE_URL_VERSION_1 = "/api/v1/traffic";
+    public static final String BASE_URL_VERSION_1 = "/v1/traffic";
 }
