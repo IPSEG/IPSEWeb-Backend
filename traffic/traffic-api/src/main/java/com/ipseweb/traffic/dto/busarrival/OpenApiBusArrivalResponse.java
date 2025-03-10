4c244be17909fc7be47288384896b7e751018a95
hoyo1744 <hoyo1744@naver.com>
2025-03-10 21:58:12 +0900

    버스 도착정보 조회 - BE [BT] CIW-38 [RV] 양승빈 - JsonDeserializer 에러 수정

--- traffic/traffic-api/src/main/java/com/ipseweb/traffic/dto/busarrival/OpenApiBusArrivalResponse.java
+++ traffic/traffic-api/src/main/java/com/ipseweb/traffic/dto/busarrival/OpenApiBusArrivalResponse.java
@@ -1,13 +1,19 @@
 package com.ipseweb.traffic.dto.busarrival;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.databind.PropertyNamingStrategies;
-import com.fasterxml.jackson.databind.annotation.JsonNaming;
-import com.ipseweb.error.Response;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
@@ -31,18 +37,20 @@ public class OpenApiBusArrivalResponse {
     @Data
     @AllArgsConstructor
     @NoArgsConstructor
-    public static class OpenApiBusArrivalHeader{
+    public static class OpenApiBusArrivalHeader {
         @JsonProperty("resultCode")
         private String resultCode;
         @JsonProperty("resultMsg")
         private String resultMsg;
     }
 
+
     @Data
     @AllArgsConstructor
     @NoArgsConstructor
-    public static class OpenApiBusArrivalBody{
+    public static class OpenApiBusArrivalBody {
         @JsonProperty("items")
+        @JsonDeserialize(using = EmptyStringAsNullDatasDeserializer.class)
         private OpenApiBusArrivalDatas items;
 
         @JsonProperty("numOfRows")
@@ -56,12 +64,48 @@ public class OpenApiBusArrivalResponse {
 
     }
 
-
     @Data
     @AllArgsConstructor
     @NoArgsConstructor
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     public static class OpenApiBusArrivalDatas {
         @JsonProperty("item")
+        @JsonDeserialize(using = EmptyStringAsNullListDeserializer.class)
         private List<OpenApiBusArrivalData> item;
+    }
+
+
+    /**
+     * OpenApiBusArrivalDatas custom JsonDeserializer
+     */
+    public static class EmptyStringAsNullDatasDeserializer extends JsonDeserializer<OpenApiBusArrivalDatas> {
+        @Override
+        public OpenApiBusArrivalDatas deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
+            JsonNode node = p.readValueAsTree();
+
+            if (node == null || node.isNull() || (node.isTextual() && node.asText().isEmpty())) {
+                return new OpenApiBusArrivalDatas(new ArrayList<>());
+            }
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            return objectMapper.treeToValue(node, OpenApiBusArrivalDatas.class);
+        }
+    }
+
+    /**
+     * List<OpenApiBusArrivalData> Custom JsonDeserializer
+     */
+    public static class EmptyStringAsNullListDeserializer extends JsonDeserializer<List<OpenApiBusArrivalData>> {
+        @Override
+        public List<OpenApiBusArrivalData> deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
+            JsonNode node = p.readValueAsTree();
+
+            if (node == null || node.isNull() || (node.isTextual() && node.asText().isEmpty())) {
+                return new ArrayList<>();
+            }
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            return objectMapper.readValue(node.toString(), objectMapper.getTypeFactory().constructCollectionType(List.class, OpenApiBusArrivalData.class));
+        }
     }
 }
